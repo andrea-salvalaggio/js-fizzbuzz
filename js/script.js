d129f4ff69359ae07197a96995694e5dc023212c
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-06-16 15:25:01 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+// 1) Creare un cliclo for che stampi in console numeri da 1 a 100
+//      - Bisogna utilizzare "for (let i = 0; i > 100; index = i--);
+// 2) Stampare in console che i multipli (divisibili) di 3 diventino "Fizz"
+// 3) Stampare in console che i multipli (divisibili) di 5 diventino "Buzz"
+//      - Ultilizzare gli "if" dentro i cicli
+
+// Per capire se un numero è divisibile per un altro utilizzare la funzione "%" (modulo)
+
+
+for (let index = 1; index <= 100; index++) {
+    console.log(index);
+
+    // if (index % 3 == 0) {
+    //     let index = ('Fizz');
+    //     console.log(index);
+    // }
+}

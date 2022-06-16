2e6e2fbbdb7db4deba9a2ce04ea63432abab9c15
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-06-16 16:54:21 +0200

    Fizz, Buzz e FizzBuzz stampati correttamente

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 // 1) Creare un cliclo for che stampi in console numeri da 1 a 100
 //      - Bisogna utilizzare "for (let i = 1; i <= 100; index = i++);
 // 2) Stampare in console che i multipli (divisibili) di 3 diventino "Fizz"
@@ -10,27 +9,18 @@
 
 
 for (let index = 1; index <= 100; index++) {
-    console.log(index);
-
-    if (index % 3 == 0) {
-        let wordFizz = 'Fizz';
-        console.log(wordFizz);
-    } else {
-
-    }
-
-    if (index % 5 == 0) {
-        let wordBuzz = 'Buzz';
-        console.log(wordBuzz);
-    } else {
-
-    }
 
     if ((index % 3 == 0) && (index % 5 == 0)) {
         let wordFizzBuzz = 'FizzBuzz';
         console.log(wordFizzBuzz);
+    } else if (index % 3 == 0) {
+        let wordFizz = 'Fizz';
+        console.log(wordFizz);
+    } else if (index % 5 == 0) {
+        let wordBuzz = 'Buzz';
+        console.log(wordBuzz);
     } else {
-
+        console.log(index);
     }
 
 }
